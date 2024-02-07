ea695bba8b0f9cf6efe3e1db838491a6db70a59c
SoftwarePete <softwarepete@gmail.com>
2024-02-07 23:55:48 +0000

    add --host flag to Vite to check mobile site

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -15,12 +15,11 @@ function SearchOrder() {
   return (
     <form onSubmit={handleSubmit}>
       <input
+        name="OrderNumber"
         placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all
-        duration-300 placeholder:text-stone-400 focus:outline-none focus:ring
-        focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
+        className="rounded-full bg-yellow-100 px-4 py-2 text-sm text-stone-400 transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
       />
     </form>
   );
