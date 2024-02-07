627c93f46334495e21298a50ae3aedc15799bfa0
SoftwarePete <softwarepete@gmail.com>
2024-02-07 23:55:43 +0000

    style(all): add styling to whole app with tailwind

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,6 @@
-import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
-import { createOrder } from '../../services/apiRestaurant';
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
+import Button from "../../ui/Button";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -10,21 +11,21 @@ const isValidPhone = (str) =>
 const fakeCart = [
   {
     pizzaId: 12,
-    name: 'Mediterranean',
+    name: "Mediterranean",
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: 'Vegetale',
+    name: "Vegetale",
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: 'Spinach and Mushroom',
+    name: "Spinach and Mushroom",
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -33,7 +34,7 @@ const fakeCart = [
 
 function CreateOrder() {
   const navigation = useNavigation();
-  const isSubmitting = navigation.state === 'submitting';
+  const isSubmitting = navigation.state === "submitting";
 
   const formErrors = useActionData();
 
@@ -44,48 +45,63 @@ function CreateOrder() {
   ///// React Router Form calls "action" onSubmit of form
 
   return (
-    <div>
+    <div className="px-4 py-6">
       {/* <Form method="POST" action="/order/new">   */}
       {/* No need to specifi the route in this case  */}
       {/* router will match closest route            */}
       <Form method="POST">
-        <h2>{"Ready to order? Let's go!"}</h2>
-        <div>
-          <label>First Name</label>
-          <input type="text" name="customer" required />
+        <h2 className="mb-8 text-xl font-semibold ">
+          {"Ready to order? Let's go!"}
+        </h2>
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <input className="input grow" type="text" name="customer" required />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input type="tel" name="phone" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input className="input w-full" type="tel" name="phone" required />
+            {formErrors?.phone && ( // TODO: hide error onChange if there is one
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {formErrors.phone}
+              </p>
+            )}
           </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
-            <input type="text" name="address" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input
+              className="input w-full"
+              type="text"
+              name="address"
+              required
+            />
           </div>
         </div>
 
-        <div>
+        <div className="mb-12 flex items-center gap-5">
           <input
             type="checkbox"
             name="priority"
             id="priority"
+            className="h-6 w-6 accent-yellow-400 focus:outline-none
+            focus:ring focus:ring-yellow-400 focus:ring-offset-2 md:px-6 md:py-3"
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor="priority" className="font-medium">
+            Want to yo give your order priority?
+          </label>
         </div>
         <div>
           {/* The hidden input will get passed along as to 'action' */}
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
-            {isSubmitting ? 'Placing order...' : 'Order now'}
-          </button>
+          <Button disabled={isSubmitting} type="primary">
+            {isSubmitting ? "Placing order..." : "Order now"}
+          </Button>
         </div>
       </Form>
     </div>
@@ -101,23 +117,23 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === 'on',
+    priority: data.priority === "on",
   };
 
   const errors = {};
   if (!isValidPhone(order.phone))
     errors.phone =
-      'Please give us your correct phone number\nWe might need it to contact you.';
+      "Please give us your correct phone number\nWe might need it to contact you.";
 
   if (Object.keys(errors).length > 0) return errors;
 
   /* if we're good, create order */
-  const newOrder = await createOrder(order);
+  // const newOrder = await createOrder(order);
 
-  /* Need to redirect to newOrder page when it's created                */
-  /* But can't call useNavigate inside functions, has to be a component */
-  /* So we use 'redirect' instead                                       */
-  return redirect(`/order/${newOrder.id}`);
+  // /* Need to redirect to newOrder page when it's created                */
+  // /* But can't call useNavigate inside functions, has to be a component */
+  // /* So we use 'redirect' instead                                       */
+  // return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
