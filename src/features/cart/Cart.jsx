440bed8d46ed83857809714dd638f65a0be0d8b3
SoftwarePete <softwarepete@gmail.com>
2024-02-07 23:55:44 +0000

    feat(state): add and config redux-toolkit

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -46,7 +46,7 @@ function Cart() {
         <Button to="/order/new" type="primary">
           Order pizzas
         </Button>
-        <Button type="secondary">Clear card</Button>
+        <Button type="secondary">Clear cart</Button>
         {/* <button>Clear cart</button> */}
       </div>
     </div>
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -8,7 +8,7 @@ function Button({ children, disabled, to, type }) {
     primary: base + " px-4 py-2 md:px-6 md:py-4",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
     secondary:
-      "inline-block text-sm rounded-full border-2 border-stone-300 font-semibold hover:bg-stone-800 uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-1.5 md:px-6 md:py-3.5",
+      "inline-block text-sm rounded-full border-2 border-stone-300 font-semibold hover:bg-stone-800uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-1.5 md:px-6 md:py-3.5",
   };
   if (to)
     return (
